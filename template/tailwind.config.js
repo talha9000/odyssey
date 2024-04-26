2ce2db95abd51ddb35a35562cc55ebdfa4748ec2
muhammad.talha <muhammad.talha@odysseyanalytics.net>
2024-04-26 12:38:30 +0500

    first

---
+++ template/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/react-tailwindcss-datepicker/dist/index.esm.js"
+  ],
+  darkMode: ["class", '[data-theme="dark"]'],
+  lightMode: ["class", '[data-theme="light"]'],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  daisyui: {
+    themes: ["light"
+    // ,"dark"
+  ],
+  },
+
+}
